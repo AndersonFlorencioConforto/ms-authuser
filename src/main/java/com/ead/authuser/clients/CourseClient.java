31cbd7c351289546177bc36e5152e227255e3fac
AndersonFlorencioConforto <afcaconforto@gmail.com>
2022-07-04 16:54:26 -0300

    implementa autorização no endpoint onde se comunica de forma assincrona com ms-course

--- src/main/java/com/ead/authuser/clients/CourseClient.java
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -4,20 +4,19 @@ import com.ead.authuser.dtos.CourseDTO;
 import com.ead.authuser.dtos.ResponsePageDTO;
 import com.ead.authuser.services.UtilsService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -34,21 +33,22 @@ public class CourseClient {
 
     //    @Retry(name = "retryInstance",fallbackMethod = "retryfallback")
     @CircuitBreaker(name = "circuitbreakerInstance")
-    public Page<CourseDTO> getAllCoursesByUserId(UUID userId, Pageable pageable) {
+    public Page<CourseDTO> getAllCoursesByUserId(UUID userId, Pageable pageable, String token) {
         List<CourseDTO> searchResult = null;
         ResponseEntity<ResponsePageDTO<CourseDTO>> result = null;
         String url = utilsService.createUrl(userId, pageable);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", token);
+        HttpEntity<String> requestEntity = new HttpEntity<>("parameters", headers);
         log.debug("Request URL : {}", url);
         log.info("Request URL : {}", url);
-        try {
-            ParameterizedTypeReference<ResponsePageDTO<CourseDTO>> responseType = new ParameterizedTypeReference<ResponsePageDTO<CourseDTO>>() {
-            };
-            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
-            searchResult = result.getBody().getContent();
-            log.debug("Response Number of Elements: {}", searchResult.size());
-        } catch (HttpStatusCodeException e) {
-            log.error("Error request /courses {}", e);
-        }
+
+        ParameterizedTypeReference<ResponsePageDTO<CourseDTO>> responseType = new ParameterizedTypeReference<ResponsePageDTO<CourseDTO>>() {
+        };
+        result = restTemplate.exchange(url, HttpMethod.GET, requestEntity, responseType);
+        searchResult = result.getBody().getContent();
+
+        log.debug("Response Number of Elements: {}", searchResult.size());
         log.info("Ending request /courses userId {}", userId);
         return result.getBody();
     }
--- src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -10,10 +10,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -28,13 +26,15 @@ public class UserCourseController {
     @Autowired
     private UserService userService;
 
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping("/users/{userId}/courses")
     public ResponseEntity<Page<CourseDTO>> getAllCoursesByUser(@PageableDefault(
             page = 0,
             size = 10,
             sort = "courseId",
-            direction = Sort.Direction.ASC) Pageable pageable, @PathVariable(value = "userId") UUID userId) {
+            direction = Sort.Direction.ASC) Pageable pageable, @PathVariable(value = "userId") UUID userId,
+                                                               @RequestHeader("Authorization") String token) {
         userService.findById(userId);
-        return ResponseEntity.ok().body(courseClient.getAllCoursesByUserId(userId, pageable));
+        return ResponseEntity.ok().body(courseClient.getAllCoursesByUserId(userId, pageable,token));
     }
 }
